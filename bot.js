2f64cd8935840e860b2743fc77eb126fe6d91e4c
H36OPigmen <44673531+H36OPigmen@users.noreply.github.com>
2019-01-08 23:47:00 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,26 +5,33 @@ const Discord = require('discord.js');
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
 const client = new Discord.Client();
-const prefix = "tt";
 
 client.on('ready', () => {
     console.log('I am ready!');
     });
+    
+    client.on('message', message => {
+        if (message.content === 'ping') {
+          message.reply('pong');
+        }
+});
 
-client.on("message", message => {
-  if (message.author.bot) return;
-  // This is where we'll put our code.
-  if (message.content.indexOf(config.prefix) !== 0) return;
- 
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
- 
-  if(command === 'ping') {
-    message.channel.send('Pong!');
-  } else
-  if (command === 'big') {
-    message.channel.send('BIG NIBBA');
-  }
+    client.on('message', message => {
+        if (message.content === 'big') {
+          message.reply('BIG NIBBA');
+        }
+});
+
+    client.on('message', message => {
+        if (message.content === 'good doggy') {
+          message.reply('Gracias Papi :D');
+        }
+});
+
+    client.on('message', message => {
+        if (message.content === 'ya') {
+          message.reply('Yeet');
+        }
 });
 
 //THIS MUST BE THIS WAY
