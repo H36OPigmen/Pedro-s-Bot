7b17a5cd8a88a667262ae7f3a2ee33154efcb95b
H36OPigmen <44673531+H36OPigmen@users.noreply.github.com>
2019-01-08 21:04:30 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ client.on("message", message => {
   } else
   if (command === 'big') {
     message.channel.send('BIG NIBBA');
-  }
+  } else
     if(command === "say"){
   let text = args.join(" ");
   message.delete();
