2728a0af56db11810034862cab6d0152f6cda1b9
H36OPigmen <44673531+H36OPigmen@users.noreply.github.com>
2019-01-09 15:25:10 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on("message", message => {
   if (command === 'big') {
     message.channel.send('BIG NIBBA');
   } else
-        if (command === 'fuck you') {
+        if (command === 'fuck') {
     message.channel.send('no u');
   }
 });
